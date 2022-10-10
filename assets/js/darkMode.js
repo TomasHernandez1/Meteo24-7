764f7e21f2be255df2fc64e6bc6f922a31cac41e
TomasHernandez1 <104203043+TomasHernandez1@users.noreply.github.com>
2022-10-10 09:13:09 +0200

    Update darkMode.js

--- assets/js/darkMode.js
+++ assets/js/darkMode.js
@@ -1,6 +1,8 @@
 function setLight () {
     document.getElementById('card').style.backgroundColor = "rgba(255, 255, 255, 0.8)"
     document.getElementById('card').style.color = "black"
+    document.getElementById('card2').style.backgroundColor = "rgba(255, 255, 255, 0.8)"
+    document.getElementById('card2').style.color = "black"
     document.getElementById('search-bar').style.backgroundColor = "rgba(135, 135, 135, 0.7)"
     document.getElementById('search').style.backgroundColor = "rgba(135, 135, 135, 0.7)"
   }
@@ -8,6 +10,8 @@ function setLight () {
   function setDark () {
     document.getElementById('card').style.backgroundColor = "#000000d0"
     document.getElementById('card').style.color = "white"
+    document.getElementById('card2').style.backgroundColor = "#000000d0"
+    document.getElementById('card2').style.color = "white"  
     document.getElementById('search-bar').style.backgroundColor = "#7c7c7c2b"
     document.getElementById('search').style.backgroundColor = "#7c7c7c2b"
   }
