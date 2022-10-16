5aebdb0250f04779d34148db6c4d9c86e9405c2a
TomasHernandez1 <104203043+TomasHernandez1@users.noreply.github.com>
2022-10-16 12:47:00 +0200

    Add files via upload

--- assets/js/darkMode.js
+++ assets/js/darkMode.js
@@ -1,17 +1,13 @@
 function setLight () {
-    document.getElementById('card').style.backgroundColor = "rgba(255, 255, 255, 0.8)"
-    document.getElementById('card').style.color = "black"
-    document.getElementById('ff').style.backgroundColor = "rgba(255, 255, 255, 0.8)"
-    document.getElementById('ff').style.color = "black"
+    document.getElementById('current-info').style.backgroundColor = "rgba(255, 255, 255, 0.8)"
+    document.getElementById('current-info').style.color = "black"
     document.getElementById('search-bar').style.backgroundColor = "rgba(135, 135, 135, 0.7)"
     document.getElementById('search').style.backgroundColor = "rgba(135, 135, 135, 0.7)"
   }
   
   function setDark () {
-    document.getElementById('card').style.backgroundColor = "#000000d0"
-    document.getElementById('card').style.color = "white"
-    document.getElementById('ff').style.backgroundColor = "#000000d0"
-    document.getElementById('ff').style.color = "white"  
+    document.getElementById('current-info').style.backgroundColor = "#000000d0"
+    document.getElementById('current-info').style.color = "white" 
     document.getElementById('search-bar').style.backgroundColor = "#7c7c7c2b"
     document.getElementById('search').style.backgroundColor = "#7c7c7c2b"
   }
