622ec32a2e06282e92292fc0842114aea93c141d
TomasHernandez1 <104203043+TomasHernandez1@users.noreply.github.com>
2022-10-16 12:50:49 +0200

    Add files via upload

--- assets/js/darkMode.js
+++ assets/js/darkMode.js
@@ -1,13 +1,17 @@
 function setLight () {
-    document.getElementById('current-info').style.backgroundColor = "rgba(255, 255, 255, 0.8)"
-    document.getElementById('current-info').style.color = "black"
+    document.getElementById('current-weather-items').style.backgroundColor = "rgba(255, 255, 255, 0.8)"
+    document.getElementById('current-weather-items').style.color = "black"
+    document.getElementById('place-container').style.backgroundColor = "rgba(255, 255, 255, 0.8)"
+    document.getElementById('place-container').style.color = "black"
     document.getElementById('search-bar').style.backgroundColor = "rgba(135, 135, 135, 0.7)"
     document.getElementById('search').style.backgroundColor = "rgba(135, 135, 135, 0.7)"
   }
   
   function setDark () {
-    document.getElementById('current-info').style.backgroundColor = "#000000d0"
-    document.getElementById('current-info').style.color = "white" 
+    document.getElementById('current-weather-items').style.backgroundColor = "#000000d0"
+    document.getElementById('current-weather-items').style.color = "white" 
+    document.getElementById('place-container').style.backgroundColor = "#000000d0"
+    document.getElementById('place-container').style.color = "white" 
     document.getElementById('search-bar').style.backgroundColor = "#7c7c7c2b"
     document.getElementById('search').style.backgroundColor = "#7c7c7c2b"
   }
