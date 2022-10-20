6243c5d745214e05e54c9a70a3d15cd18baafa9f
TomasHernandez1 <104203043+TomasHernandez1@users.noreply.github.com>
2022-10-20 09:55:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const app = express()
-const Ddos = require('ddos')
-var ddos = new Ddos ({burst:10, limit:10})
 
 //Import routes
 const routes = require('./routes.js')
 
 app.use(express.urlencoded({ extended: true }))
-app.use(ddos.express)
 
 //Use view engine
 app.set('view engine', 'ejs')
